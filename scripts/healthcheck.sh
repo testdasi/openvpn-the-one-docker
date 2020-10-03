54a8be966c0b92854ae774e7659d790c94c0e849
testdasi <51292604+testdasi@users.noreply.github.com>
2020-10-03 21:30:12 +0100

    Update healthcheck.sh

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -66,6 +66,13 @@ then
     /app/nzbhydra2/nzbhydra2 --daemon --nobrowser --java /usr/lib/jvm/java-11-openjdk-amd64/bin/java --datafolder /root/nzbhydra2 --pidfile /root/nzbhydra2/nzbhydra2.pid
 fi
 
+pidlist=$(pidof jackett)
+if [ -z "$pidlist" ]
+then
+    crashed=$(( $crashed + 1 ))
+    start-stop-daemon --start --background --chuid nobody --name jackett --chdir /app/jackett --exec /app/jackett/jackett -- --DataFolder=/root/jackett
+fi
+
 pidlist=$(pidof python3)
 if [ -z "$pidlist" ]
 then
