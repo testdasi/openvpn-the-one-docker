549c776c99e34d4d6d3a6a8cb11e7b75b0d10296
testdasi <51292604+testdasi@users.noreply.github.com>
2020-10-03 18:23:21 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,8 +1,80 @@
 #!/bin/bash
 
-sleep_time=3600
-while true
-do
-    echo 'All your base are belong to us'
-    sleep $sleep_time
-done
+### Only run process if ovpn found ###
+if [[ -f "/root/openvpn/openvpn.ovpn" ]]
+then
+    echo '[info] Config file detected...'
+    ### Set various variable values ###
+    echo ''
+    echo '[info] Setting variables'
+    source /set_variables.sh
+    echo '[info] All variables set'
+
+    ### Fixing config files ###
+    echo ''
+    echo '[info] Fixing configs'
+    source /fix_config.sh
+    echo '[info] All configs fixed'
+
+    ### Stubby DNS-over-TLS ###
+    echo ''
+    echo "[info] Run stubby in background on port $DNS_PORT"
+    stubby -g -C /root/stubby/stubby.yml
+    ipnaked=$(dig +short myip.opendns.com @208.67.222.222)
+    echo "[warn] Your ISP public IP is $ipnaked"
+
+    ### nftables ###
+    echo ''
+    echo '[info] Set up nftables rules'
+    source /nftables.sh
+    echo '[info] All rules created'
+
+    ### OpenVPN ###
+    echo ''
+    echo "[info] Setting up OpenVPN tunnel"
+    source /static/scripts/openvpn.sh
+    echo '[info] Done'
+
+    ### Dante SOCKS proxy to VPN ###
+    echo ''
+    echo "[info] Run danted in background on port $DANTE_PORT"
+    danted -D -f /root/dante/danted.conf
+
+    ### Tinyproxy HTTP proxy to VPN ###
+    echo ''
+    echo "[info] Run tinyproxy in background with no log on port $TINYPROXY_PORT"
+    tinyproxy -c /root/tinyproxy/tinyproxy.conf
+
+    ### sabnzbdplus
+    echo ''
+    echo "[info] Run sabnzbdplus in background on HTTP port $SAB_PORT_A and HTTPS port $SAB_PORT_B"
+    sabnzbdplus --daemon --config-file /root/sabnzbdplus/sabnzbdplus.ini --pidfile /root/sabnzbdplus/sabnzbd.pid
+
+    ### rtorrent + flood
+    echo ''
+    echo "[info] Run rtorrent and flood in background on port $FLOOD_PORT"
+    screen -d -m -fa -S rtorrent /usr/bin/rtorrent
+    start-stop-daemon --start --background --name flood --chdir /app/flood --exec /app/flood/flood.sh
+
+    ### nzbhydra2
+    echo ''
+    echo "[info] Run nzbhydra2 in background on port $HYDRA_PORT"
+    /app/nzbhydra2/nzbhydra2 --daemon --nobrowser --java /usr/lib/jvm/java-11-openjdk-amd64/bin/java --datafolder /root/nzbhydra2 --pidfile /root/nzbhydra2/nzbhydra2.pid
+
+    ### GUI launcher
+    echo ''
+    echo "[info] Run WebUI launcher in background at $LAUNCHER_IP:$LAUNCHER_PORT"
+    start-stop-daemon --start --background --name launcher --chdir /app/launcher --exec /app/launcher/launcher-python3.sh
+
+    ### Periodically checking IP ###
+    sleep_time=3600
+    echo ''
+    while true
+    do
+        iphiden=$(dig +short myip.opendns.com @208.67.222.222)
+        echo "[info] Your VPN public IP is $iphiden"
+        sleep $sleep_time
+    done
+else
+    echo '[CRITICAL] Config file not found, quitting...'
+fi
