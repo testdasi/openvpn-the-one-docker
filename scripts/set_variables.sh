b1f636354c6401457897d5926460bcd99e5105c5
testdasi <51292604+testdasi@users.noreply.github.com>
2020-10-03 18:20:25 +0100

    Create set_variables.sh

---
+++ scripts/set_variables.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+### DNS and DoT ports are fixed ###
+echo '[info] Set various ports to docker variables'
+LAUNCHER_PORT=${LAUNCHER_GUI_PORT}
+LAUNCHER_IP=${SERVER_IP}
+DNS_PORT=${DNS_SERVER_PORT}
+DANTE_PORT=${SOCKS_PROXY_PORT}
+TINYPROXY_PORT=${HTTP_PROXY_PORT}
+SAB_PORT_A=${USENET_HTTP_PORT}
+SAB_PORT_B=${USENET_HTTPS_PORT}
+FLOOD_PORT=${TORRENT_GUI_PORT}
+HYDRA_PORT=${SEARCHER_GUI_PORT}
+SONARR_PORT=${PVR_TV_PORT}
+RADARR_PORT=${PVR_MOVIE_PORT}
+JACKETT_PORT=${TORZNAB_PORT}
+# DoT port is fixed due to TLS protocol
+DOT_PORT=853
+
+### static scripts ###
+source /static/scripts/set_variables_ovpn_port_proto.sh
+source /static/scripts/set_variables_eth0.sh
