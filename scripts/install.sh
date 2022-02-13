104a403f170a66ac2e8270d8c8c2f286b8bd9740
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-13 14:09:20 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,14 +18,14 @@ mkdir -p /temp \
     && rm -rf /temp
 
 # fix static files for repo-specific stuff
-sed -i "s|\/data\/deluge\/watch|\/data\/rtorrent\/watch|g" '/static/config/nzbhydra.yml'
+sed -i "s|\/data\/deluge\/watch|\/data\/transmission\/watch|g" '/static/config/nzbhydra.yml'
 sed -i "s|\/etc\/openvpn|\/root\/openvpn|g" '/static/scripts/openvpn.sh'
 sed -i "s|\/etc\/openvpn|\/root\/openvpn|g" '/static/scripts/set_variables_ovpn_port_proto.sh'
 sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_stubby.sh'
 sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_dante.sh'
 sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_tinyproxy.sh'
 sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_sabnzbdplus.sh'
-sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_rtorrent.sh'
+sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_transmission.sh'
 sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_nzbhydra2.sh'
 sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_sonarr.sh'
 sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_radarr.sh'
